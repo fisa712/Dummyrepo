ed218ff1814c44867d87d8c2d1b195fb3db9d3ed
fisa712 <101712610+fisa712@users.noreply.github.com>
2023-02-23 21:35:00 +0500

    Create wallet.py

---
+++ wallet.py
@@ -0,0 +1,19 @@
+import numpy as np 
+import pandas as pd
+
+class Wallet:
+  def __init__(self):
+    self.balance = np.array(0)
+   
+  def setAmount(self, amount):
+    self.balance = np.array(amount)
+    
+  def getAmount(self):
+    return self.balance
+  
+  def removeAmount(self, withdrawal):
+    if self.balance>=withdrawal:
+      self.balance -= withdrawal
+    else:
+       return 'Low Amount'
+    return 'Succesfully Done'
